ec25d8dd0f991d10e4f2efdad2e852761491ddc3
pavle tabatadze <tabatadzepavle@gmail.com>
2016-09-29 00:14:05 +0400

    დანამატი

---
+++ Sangu/src/Sangu.java
@@ -0,0 +1,12 @@
+/**
+ * Created by pavle on 9/28/2016.
+ */
+public class Sangu {
+    public static void main(String [] args){
+        System.out.println("Hello Sangu 2016!");
+        for (int i = 0; i<10; i++){
+            System.out.println(i);
+	    System.out.println(i++);
+        }
+    }
+}
